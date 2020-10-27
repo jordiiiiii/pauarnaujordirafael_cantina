dc6eddba13175761917997b71c37b21982d82c06
jordi <a18jorgornei@inspedralbes.cat>
2020-10-27 13:52:50 +0100

    petits canvis son poderosos

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inici</title>
+    <title>Inicii</title>
     <link rel = "stylesheet" href = "style_general.css">
 </head>
 <body>
