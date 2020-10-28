deedd698d162c115040966b13aa80632e4deb533
jordi <a18jorgornei@inspedralbes.cat>
2020-10-28 10:36:18 +0100

    segur, no anira

--- header.php
+++ header.php
@@ -38,32 +38,4 @@
         <!-- ============================================================== -->
         <!-- Start Page Content here -->
         <!-- ============================================================== -->
-
-
-
-
-        
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicii</title>
-    <link rel = "stylesheet" href = "style_general.css">
-</head>
-<body>
-    <header>
-        <div class = "empaquetador">
-            <div class = "logo">
-                <a href = "#"><img src = "img/logo.jpg" alt = "FalconMasters"></a>
-            </div>
-            <nav>
-                <ul>
-                    <li><a href = "index.php">Inici</a></li>
-                    <li><a href = "menu.php">Menu</a></li>
-                    <li><a href = "contacte.php">Contacte</a></li>
-                </ul>
-            </nav>
-    </header>
-</body>
-</html> -->
+  
